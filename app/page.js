aa2e99a4ca17c85b469b0713b92ab8d73272fa9d
Debajyoti <chatterjeedebajyoti254@gmail.com>
2025-02-02 11:05:01 +0530

    hero fixes

--- app/page.js
+++ app/page.js
@@ -1,14 +1,9 @@
-import Image from "next/image";
-import { BackgroundGradientAnimationDemo } from "@/components/background";
-import { TypewriterEffectDemo } from "@/components/text-effect-component";
-import { BackgroundBeamsDemo } from "@/components/background-beams-demo";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      {/* <h1> Coming soon</h1> */}
     </>
   );
 }
